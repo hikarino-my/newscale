a3319b3ce7a8a3e757b094d5ec5ff1319a898ee0
Masataka Yoshimura <26180238+hikarino-my@users.noreply.github.com>
2024-10-04 07:27:26 +1100

    initialize chime

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ class Synth {
   hz = 440;
   type = "sine";
   pars = [1, 1];
-  bufferLength;
   dataArray;
   constructor({ keyCode, pars, type }) {
     //  "sine", "square", "sawtooth", "triangle"
     if (type == "sawtooth") {
       type = "sine";
     }
-    type = "sine";
+    type = "triangle";
     this.type = type;
     this.pars = pars;
     this.keyCode = keyCode;
@@ -37,9 +36,6 @@ class Synth {
     this.oscillator.start(0);
   }
   playSound(code) {
-    //this.stopSound();
-    // Initialize AudioContext
-    //console.log('Sound playing');
     if (this.stopped) {
       if (!this.gainNode) {
         this.gainNode = audioContext.createGain();
@@ -50,6 +46,7 @@ class Synth {
         this.createOs();
         this.gainNode.connect(analyser);
       } else {
+        // TODO: consider discarding gainNode
         this.gainNode.gain.cancelScheduledValues(audioContext.currentTime);
         this.oscillator.stop(audioContext.currentTime);
         this.gainNode.gain.setValueAtTime(
@@ -142,37 +139,28 @@ function drawScale() {
   const canvasCtx = canvas.getContext("2d");
   canvas.width = canvas.clientWidth;
   canvas.height = canvas.clientHeight;
-
   function draw() {
-    const timestamp = new Date();
-    const delta = Math.floor(timestamp - now);
     requestAnimationFrame(draw); // Call draw at the next animation frame
     canvasCtx.strokeStyle = "#333"; // 塗りつぶしは暗めの色
     canvasCtx.fillStyle = "#f00"; // 線は赤色
     canvasCtx.lineWidth = 5; // 線の幅は5px
     canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-    canvasCtx.beginPath();
     const octwidth = canvas.width / 7;
     const keywidth = octwidth / 12;
     const A440 = octwidth * 3 + keywidth * 9.5;
-    const target = syns
+    syns
       .filter((s) => !s.stopped)
       .map((s) => s.hz)
       .map((v) => Math.log2(v / 440))
       .map((v) => v * octwidth + A440)
       .forEach((v) => {
+        canvasCtx.beginPath();
         // const x = octwidth * 3 + keywidth * 9.5; // x 座標
         const y = (canvas.height * 2) / 3; // y 座標
         canvasCtx.arc(v, y, 5, 0, 2 * Math.PI);
         canvasCtx.closePath();
         canvasCtx.fill();
       });
-    //console.log(x, target[0]);
-    //canvasCtx.lineWidth = 2;
-    //canvasCtx.strokeStyle = "rgb(0, 0, 0)";
-    //canvasCtx.beginPath();
-    //canvasCtx.lineTo(canvas.width, canvas.height / 2);
-    //canvasCtx.stroke(); // Draw the waveform
   }
   draw();
 }
@@ -189,94 +177,98 @@ F (ファ) = 440 × 2^(8/12) ≈ 698.46Hz
 F♯/G♭ (ファ♯/ソ♭) = 440 × 2^(9/12) ≈ 739.99Hz
 G (ソ) = 440 × 2^(10/12) ≈ 783.99Hz
 G♯/A♭ (ソ♯/ラ♭) = 440 × 2^(11/12) ≈ 830.61Hz
+const syn1 = new Synth({hz: ref * Math.pow(2,   (-9/12)), keyCode: "Digit1"})
 */
 const ref = 440;
-/*const syn1 = new Synth({hz: ref * Math.pow(2,   (-9/12)), keyCode: "Digit1"})
-const syn2 = new Synth({hz: ref * Math.pow(2,   (-8/12)), keyCode: "Digit2"})
-const syn3 = new Synth({hz: ref * Math.pow(2,   (-7/12)), keyCode: "Digit3"})
-const syn4 = new Synth({hz: ref * Math.pow(2,   (-6/12)), keyCode: "Digit4"})
-const syn5 = new Synth({hz: ref * Math.pow(2,   (-5/12)), keyCode: "Digit5"})
-const syn6 = new Synth({hz: ref * Math.pow(2,   (-4/12)), keyCode: "Digit6"})
-const syn7 = new Synth({hz: ref * Math.pow(2,   (-3/12)), keyCode: "Digit7"})
-const syn8 = new Synth({hz: ref * Math.pow(2,   (-2/12)), keyCode: "Digit8"})
-const syn9 = new Synth({hz: ref * Math.pow(2,   (-1/12)), keyCode: "Digit9"})
-const syn10 = new Synth({hz: ref * Math.pow(2,  (0/12)), keyCode: "Digit0"})
-const syn11 = new Synth({hz: ref * Math.pow(2,  (1/12)), keyCode: "Minus"})
-const syn12 = new Synth({hz: ref * Math.pow(2,  (2/12)), keyCode: "Equal"})
-*/
 const type = "triangle"; // "sine", "square", "sawtooth", "triangle"
-const syns = [
-  new Synth({ type: "sine", pars: [1, 1], keyCode: "Digit1" }),
-  new Synth({ type: "triangle", pars: [2, 1], keyCode: "Digit2" }),
-  new Synth({ type: "square", pars: [3, 2], keyCode: "Digit3" }),
-  new Synth({ type: "sawtooth", pars: [4, 3], keyCode: "Digit4" }),
-  new Synth({ type: "sine", pars: [5, 4], keyCode: "Digit5" }),
-  new Synth({ type: "square", pars: [5, 3], keyCode: "Digit6" }),
-  new Synth({ type: "sawtooth", pars: [6, 5], keyCode: "Digit7" }),
-  new Synth({ type: "sawtooth", pars: [7, 6], keyCode: "Digit8" }),
-  new Synth({ type: "triangle", pars: [7, 5], keyCode: "Digit9" }),
-  new Synth({ type: "sawtooth", pars: [7, 4], keyCode: "Digit0" }),
-  new Synth({ type: "sine", pars: [8, 7], keyCode: "Minus" }),
-  new Synth({ type: "triangle", pars: [8, 5], keyCode: "Equal" }),
-  new Synth({ type: "sawtooth", pars: [9, 8], keyCode: "IntlYen" }),
-
-  new Synth({ type: "sine", pars: [9, 7], keyCode: "KeyQ" }),
-  new Synth({ type: "sawtooth", pars: [9, 5], keyCode: "KeyW" }),
-  new Synth({ type: "square", pars: [10, 9], keyCode: "KeyE" }),
-  new Synth({ type: "sine", pars: [10, 7], keyCode: "KeyR" }),
-  new Synth({ type: "triangle", pars: [11, 10], keyCode: "KeyT" }),
-  new Synth({ type: "sawtooth", pars: [11, 9], keyCode: "KeyY" }),
-  new Synth({ type: "square", pars: [11, 8], keyCode: "KeyU" }),
-  new Synth({ type: "square", pars: [11, 7], keyCode: "KeyI" }),
-  new Synth({ type: "sawtooth", pars: [11, 6], keyCode: "KeyO" }),
-  new Synth({ type: "sawtooth", pars: [12, 11], keyCode: "KeyP" }),
-  new Synth({ type: "triangle", pars: [12, 7], keyCode: "BracketLeft" }),
-  new Synth({ type: "square", pars: [1, 2], keyCode: "BracketRight" }),
-
-  new Synth({ type: "square", pars: [2, 3], keyCode: "KeyA" }),
-  new Synth({ type: "triangle", pars: [3, 4], keyCode: "KeyS" }),
-  new Synth({ type: "sine", pars: [3, 5], keyCode: "KeyD" }),
-  new Synth({ type: "triangle", pars: [4, 5], keyCode: "KeyF" }),
-  new Synth({ type: "triangle", pars: [4, 7], keyCode: "KeyG" }),
-  new Synth({ type: "triangle", pars: [5, 6], keyCode: "KeyH" }),
-  new Synth({ type: "sine", pars: [5, 7], keyCode: "KeyJ" }),
-  new Synth({ type: "square", pars: [5, 8], keyCode: "KeyK" }),
-  new Synth({ type: "sawtooth", pars: [5, 9], keyCode: "KeyL" }),
-  new Synth({ type: "sine", pars: [6, 7], keyCode: "Semicolon" }),
-  new Synth({ type: "sine", pars: [7, 8], keyCode: "Quote" }),
-  new Synth({ type: "sine", pars: [7, 9], keyCode: "Backslash" }),
-
-  new Synth({ type: "square", pars: [7, 10], keyCode: "KeyZ" }),
-  new Synth({ type: "sine", pars: [7, 11], keyCode: "KeyX" }),
-  new Synth({ type: "square", pars: [7, 12], keyCode: "KeyC" }),
-  new Synth({ type: "sawtooth", pars: [7, 13], keyCode: "KeyV" }),
-  new Synth({ type: "triangle", pars: [8, 9], keyCode: "KeyB" }),
-  new Synth({ type: "square", pars: [8, 11], keyCode: "KeyN" }),
-  new Synth({ type: "sawtooth", pars: [8, 13], keyCode: "KeyM" }),
-  new Synth({ type: "sine", pars: [9, 10], keyCode: "Comma" }),
-  new Synth({ type: "triangle", pars: [9, 11], keyCode: "Period" }),
-  new Synth({ type: "square", pars: [9, 13], keyCode: "Slash" }),
-];
-
-let stared = false;
+const tar = {
+  Digit1: [1, 1],
+  Digit2: [2, 1],
+  Digit3: [3, 2],
+  Digit4: [4, 3],
+  Digit5: [5, 4],
+  Digit6: [5, 3],
+  Digit7: [6, 5],
+  Digit8: [7, 6],
+  Digit9: [7, 5],
+  Digit0: [7, 4],
+  Minus: [8, 7],
+  Equal: [8, 5],
+  IntlYen: [9, 8],
+  KeyQ: [9, 7],
+  KeyW: [9, 5],
+  KeyE: [10, 9],
+  KeyR: [10, 7],
+  KeyT: [11, 10],
+  KeyY: [11, 9],
+  KeyU: [11, 8],
+  KeyI: [11, 7],
+  KeyO: [11, 6],
+  KeyP: [12, 11],
+  BracketLeft: [12, 7],
+  BracketRight: [1, 2],
+  KeyA: [2, 3],
+  KeyS: [3, 4],
+  KeyD: [3, 5],
+  KeyF: [4, 5],
+  KeyG: [4, 7],
+  KeyH: [5, 6],
+  KeyJ: [5, 7],
+  KeyK: [5, 8],
+  KeyL: [5, 9],
+  Semicolon: [6, 7],
+  Quote: [7, 8],
+  Backslash: [7, 9],
+  KeyZ: [7, 10],
+  KeyX: [7, 11],
+  KeyC: [7, 12],
+  KeyV: [7, 13],
+  KeyB: [8, 9],
+  KeyN: [8, 11],
+  KeyM: [8, 13],
+  Comma: [9, 10],
+  Period: [9, 11],
+  Slash: [9, 13],
+};
+const syns = Object.entries(tar).map(
+  ([keyCode, pars]) => new Synth({ type: "sine", pars, keyCode })
+);
+let started = false;
 
 window.addEventListener("keydown", (event) => {
   //event.preventDefault()
-  if (stared) {
+  if (started) {
     for (let syn of syns) {
       if (event.code === syn.keyCode) {
         syn.playSound(event.code);
         break;
       }
     }
-    if (event.code === "Space") {
-      current_hz = 440;
-    }
+    if (event.code === "Space") current_hz = 440;
+    if (event.code === "Escape") syns.forEach((syn) => syn.stopSound());
   } else {
     if (event.code === "Space") {
       audioContext.resume();
       console.log("resumed");
-      stared = true;
+      started = true;
+      let initialize_i = 0;
+      current_hz = 110;
+      const limit = 20;
+      let flag = true;
+      const initialize = (prev) => {
+        prev.stopSound();
+        if (initialize_i > limit) {
+          current_hz = 440;
+          return;
+        }
+        initialize_i++;
+        const syn = flag ? syns[6] : syns[17];
+        flag = !flag;
+        syn.playSound("initialize");
+        setTimeout(() => initialize(syn), 100);
+      };
+      initialize(syns[6]);
+      // syn.playSound(event.code);
     }
   }
 });
@@ -295,11 +287,13 @@ window.addEventListener("DOMContentLoaded", () => {
   drawScale();
 });
 window.onresize = () => {
-  [
-    document.getElementById("waveformCanvas"),
-    document.getElementById("scale-bar"),
-  ].forEach((canvas) => {
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
-  });
+  ["waveformCanvas", "scale-bar"]
+    .map(document.getElementById)
+    .forEach(
+      (canvas) =>
+        ([canvas.width, canvas.height] = [
+          canvas.clientWidth,
+          canvas.clientHeight,
+        ])
+    );
 };
